7151f2b39ea58e3b1babf18503d6f24e50cb8a18
rafelafrance <raphael.lafrance@gmail.com>
2022-04-29 14:08:26 -0400

    Inital commit

---
+++ .dev_env.bash
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+if [[ ! -z "$VIRTUAL_ENV" ]]; then
+  echo "'deactivate' before running this script."
+  exit 1
+fi
+
+rm -rf .venv
+python3.10 -m venv .venv
+
+source ./.venv/bin/activate
+
+python -m pip install --upgrade pip setuptools wheel
+
+
+# ##############################################################################
+# I want to use the GPU when possible.
+
+# pip3 install -U torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113
+
+# ##############################################################################
+# Install normal requirements
+
+if [ -f requirements.txt ]; then python -m pip install -r requirements.txt; fi
+
+
+# ##############################################################################
+# Dev only pip installs (not required because they're personal preference)
+
+python -m pip install -U tensorboard
+python -m pip install -U pynvim
+python -m pip install -U 'python-lsp-server[all]'
+python -m pip install -U pre-commit pre-commit-hooks
+python -m pip install -U autopep8 flake8 isort pylint yapf pydocstyle black
+python -m pip install -U jupyter jupyter_nbextensions_configurator ipyparallel
+python -m pip install -U jupyter_nbextensions_configurator jupyterlab_code_formatter
+python -m pip install -U jupyterlab-git==0.36.0
+
+python -m pip install -U jupyterlab
+python -m pip install -U jupyterlab_code_formatter
+python -m pip install -U jupyterlab-drawio
+python -m pip install -U jupyterlab-lsp
+python -m pip install -U jupyterlab-spellchecker
+python -m pip install -U jupyterlab-git
+python -m pip install -U aquirdturtle-collapsible-headings
+python -m pip install -U nbdime
+
+jupyter labextension install jupyterlab_onedarkpro
+jupyter server extension enable --py jupyterlab_git
+jupyter serverextension enable --py jupyterlab_code_formatter
+
+
+# ##############################################################################
+# I Run pre-commit hooks (optional)
+
+pre-commit install
---
+++ README.md
@@ -0,0 +1,7 @@
+# Ancient DNA![Python application](https://github.com/rafelafrance/adna/workflows/CI/badge.svg)
+
+Utilities for cleaning and working with ancient DNA.
+
+## Project Summary
+
+## Setup
