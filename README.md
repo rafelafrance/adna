4a3055f1ba8c6f75dfaaa6b2cfe2cc8b54f3e684
rafelafrance <raphael.lafrance@gmail.com>
2022-05-24 09:49:36 -0400

    Train model

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Ancient DNA![Python application](https://github.com/rafelafrance/adna/workflows/CI/badge.svg)
-[![CI](https://github.com/rafelafrance/adna/actions/workflows/python-app.yml/badge.svg)](https://github.com/rafelafrance/adna/actions/workflows/python-app.yml)
+# Ancient DNA
+
+![Python application](https://github.com/rafelafrance/adna/workflows/CI/badge.svg)
 
 Utilities for cleaning and working with ancient DNA.
 
--- adna/pylib/consts.py
+++ adna/pylib/consts.py
@@ -18,7 +18,7 @@ VECTOR_SIZE = 128  # Keeping things small for now
 # #########################################################################
 # Special tokens
 
-BOS = "<s>"  # Beginning of sequence
+BOS = "<s>"  # Beginning of sequence -- Also holds inferred class label
 EOS = "</s>"  # End of sequence
 PAD = "<pad>"  # Sequence padding to get to a uniform SEQ_LENGTH
 UNK = "<unk>"  # Unknown token -- Currently unused
@@ -29,3 +29,6 @@ SPECIAL_TOKENS = [BOS, PAD, EOS, UNK, MASK]
 # #########################################################################
 SUB_DIR = DATA_DIR / PREFIX
 SQL = SUB_DIR / f'{PREFIX}.sqlite'
+
+# select *, row_number() over win as idx
+# from seqs window win as (order by random()) limit 1000000
