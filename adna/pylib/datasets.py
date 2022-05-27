d4c78a61cc971d5fee4ba7909801767306a82088
rafelafrance <raphael.lafrance@gmail.com>
2022-05-27 00:49:22 -0400

    Update dataset for new data format

--- adna/pylib/datasets.py
+++ adna/pylib/datasets.py
@@ -12,21 +12,17 @@ from . import consts
 
 
 class ADnaDataset(data.Dataset):
-    def __init__(self, split, tokenizer, limit=-1):
+    def __init__(self, split, tokenizer):
         """
         split = "train", "val", "test" from the database
         tokenizer = a previously trained Hugging Face BPE tokenizer
-        limit = limit the records in the dataset. -1 == no limit
         """
         self.split = split
         self.check_split()
 
         self.tokenizer = tokenizer
-        self.limit = limit
 
-        self.length = 0
-        self.cxn = sqlite3.connect(":memory:")
-        self.sample_data()
+        self.data = []
 
     def check_split(self):
         with sqlite3.connect(consts.SQL) as cxn:
@@ -37,30 +33,17 @@ class ADnaDataset(data.Dataset):
                 f"Split {self.split} is not in the database.\nOptions are: {splits}"
             )
 
-    def sample_data(self):
-        create = """
-            create table seqs as
-            select seq, label, rev
-            from aux.seqs where split = ?
-            order by random()
-            limit ?;
-            """
-        self.cxn.execute("drop table if exists seqs;")
-        self.cxn.execute(f"attach database '{consts.SQL}' as aux")
-        self.cxn.execute(create, (self.split, self.limit))
-        self.cxn.execute("detach database aux")
-        count = self.cxn.execute("select count(*) from seqs")
-        self.length = count.fetchone()[0]
+    def read_data(self):
+        sql = "select seq, label, rev from seqs where split = ? order by random()"
+        with sqlite3.connect(consts.SQL) as cxn:
+            self.data = [r for r in cxn.execute(sql, (self.split,))]
 
     def __len__(self):
-        return self.length
+        return len(self.data)
 
-    def __getitem__(self, row_id):
-        row_id += 1
-        sql = "select * from seqs where rowid = ?"
-        row = self.cxn.execute(sql, (row_id,)).fetchone()
+    def __getitem__(self, idx):
         encoded = self.tokenizer.encode_plus(
-            row[0], padding="max_length", max_length=consts.MAX_LENGTH
+            self.data[idx][0], padding="max_length", max_length=consts.MAX_LENGTH
         )
-        encoded["label"] = torch.tensor(row[1])
+        encoded["label"] = torch.tensor(self.data[idx][1])
         return encoded
